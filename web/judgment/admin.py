0544dbd5306e3cc88a3b0bffe8841ae9f93bf970
Mahsa Seifikar <mahsa.seifikar@uwaterloo.ca>
2022-07-25 17:09:44 -0400

    update judgment admin

--- web/judgment/admin.py
+++ web/judgment/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
+from document.models import Document
 from .models import Judgment, JudgmentConsistency
 
 @admin.register(Judgment)
 class JudgmentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'view_user', 'parent_id', 'view_task', 'view_left_response', 'view_right_response',
-        # 'is_initialized',
-        'best_answers',
-         'is_round_done', 'is_complete', 'has_changed', 'is_tested',
+    list_display = ('id', 'view_user', 'parent_id', 'view_task', 
+        'view_left_response', 'view_right_response', 'view_best_answer', 
+        'is_round_done', 'is_complete', 'has_changed', 'is_tested',
         'action', 'created_at'
     )
 
@@ -35,6 +35,24 @@ class JudgmentAdmin(admin.ModelAdmin):
             return None
         url = reverse("admin:document_response_change", args=(obj.right_response.id,))
         return format_html('<a href="{}">({})</a>', url, obj.right_response.document.uuid)
+    
+    def view_best_answer(self, obj):
+        if not obj.best_answers:
+            return None
+        best_answer = obj.best_answers.split('--')[1:]
+        admin_best_ans = ""
+        for i, ans in enumerate(best_answer):
+            temp = ans.split("|")[:-1]
+            if temp:
+                admin_best_ans += f'\n\n Grade {i+1}:\n\n'
+            for doc in temp:
+                d = Document.objects.get(uuid=doc)
+                url = reverse("admin:document_document_change", args=(d.id,))
+                admin_best_ans += f"(<a href={url}>{doc}</a>)"
+        
+        return format_html(admin_best_ans)
+
+    view_best_answer.short_description = "Best Answer"
 
     view_user.short_description = "user"
     view_task.short_description = "task"
