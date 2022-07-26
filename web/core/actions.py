1650203c0b5008d430dd2821e95fef4ca7a9edaf
Mahsa Seifikar <mahsa.seifikar@uwaterloo.ca>
2022-07-26 12:39:52 -0400

    change actions class

--- web/core/actions.py
+++ web/core/actions.py
@@ -3,14 +3,9 @@ from django.http import HttpResponse
 
 def export_task_as_csv_action(description="Export selected objects as CSV file", fields=None, exclude=None, header=True):
     """
-    This function returns an export csv action
-    'fields' and 'exclude' work like in django ModelForm
-    'header' is whether or not to output the column names as the first row
     """
     def export_as_csv(modeladmin, request, queryset):
         """
-        Generic csv export admin action.
-        based on http://djangosnippets.org/snippets/1697/
         """
         opts = modeladmin.model._meta
         field_names = set([field.name for field in opts.fields])
