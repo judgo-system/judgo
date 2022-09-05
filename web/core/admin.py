614906031f41954e64bfc2e99039f449ae509fc7
Mahsa Seifikar <mahsa.seifikar@uwaterloo.ca>
2022-09-05 17:16:39 -0400

    add uploading tasks from csv file in admin pannel

--- web/core/admin.py
+++ web/core/admin.py
@@ -1,14 +1,31 @@
-import uuid
+
+from django import forms
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
+from django.urls import path
+from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Task
 from document.models import Document
+from topic.models import Topic
+from user.models import User
 from .actions import export_task_as_csv_action
+
+
+class CsvImportForm(forms.Form):
+    csv_upload = forms.FileField()
+
+
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
+    
+
     actions = [export_task_as_csv_action("CSV Export", fields=['id', 'user__username'])]
+
     list_display = ('id', 'view_user', 'view_topic', 'is_completed', 'num_ans', 'font_size',
         'view_best_answer', 'tags', 'created_at')
 
@@ -43,3 +60,43 @@ class TaskAdmin(admin.ModelAdmin):
     view_user.short_description = "user"
     view_topic.short_description = "topic"
     view_best_answer.short_description = "Best Answer"
+
+
+
+
+    def get_urls(self):
+        urls = super().get_urls()
+        new_urls = [path('upload-csv/', self.upload_csv),]
+        return new_urls + urls
+
+    def upload_csv(self, request):
+
+        if request.method == "POST":
+            csv_file = request.FILES["csv_upload"]
+            
+            if not csv_file.name.endswith('.csv'):
+                messages.warning(request, 'The wrong file type was uploaded')
+                return HttpResponseRedirect(request.path_info)
+            
+            file_data = csv_file.read().decode("utf-8")
+            csv_data = file_data.split("\n")[1:]
+            print(csv_data)
+
+            for i, x in enumerate(csv_data):
+                fields = x.split(",")
+                try:
+                    user = User.objects.get(username=fields[0].strip())
+                    topic = Topic.objects.get(uuid=fields[1].strip())
+                    created = Task.objects.create(
+                        user = user,
+                        topic = topic,
+                        )
+                except Exception as e:
+                    print(f"Row {i} cann't be ingested error message is {e}")
+
+            url = reverse('admin:index')
+            return HttpResponseRedirect(url)
+
+        form = CsvImportForm()
+        data = {"form": form}
+        return render(request, "admin/csv_upload.html", data)
