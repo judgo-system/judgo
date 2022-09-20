ea7703bd5d2efac654d02a17ac00ab7ac7731916
Mahsa Seifikar <mahsa.seifikar@uwaterloo.ca>
2022-09-20 15:42:37 -0400

    fix bug

--- web/settings.py
+++ web/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['judgo-system.herokuapp.com', '127.0.0.1', '0.0.0.0']
